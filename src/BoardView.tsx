bcfe565728b3a39d3db50a704827625861331028
Thanh Binh Le <ltbinh3999@gmail.com>
2021-02-22 15:06:00 +0700

    Preserve order when moving task in individual list

--- src/BoardView.tsx
+++ src/BoardView.tsx
@@ -1,23 +1,59 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState } from "react";
 import { DataProvider, getBoard } from "./data";
 import ListView from "./ListView";
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  DropResult,
+} from "react-beautiful-dnd";
 interface Props {
   boardId: string;
 }
 
 export default function BoardView({ boardId }: Props): ReactElement {
   const { name, lists, tasks, listIds } = getBoard(boardId);
+  const [data, setData] = useState({ lists, tasks });
   const listsViews = Array.from(lists.values(), (v, k) => (
     <ListView key={v.id} id={v.id} />
   ));
-  const onDragEnd = () => {
-    console.log("end");
+  const onDragEnd = (result: DropResult) => {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (destination.droppableId === source.droppableId) {
+      if (destination.index === source.index) {
+        return;
+      } else {
+        const list = data.lists.get(source.droppableId);
+        if (list) {
+          console.log(draggableId, source.index, destination.index);
+          const taskId = Array.from(list.taskIds);
+          console.log(taskId);
+          taskId.splice(source.index, 1);
+          console.log(taskId);
+          taskId.splice(destination.index, 0, draggableId);
+          console.log(taskId);
+
+          lists.set(source.droppableId, {
+            name: list.name,
+            id: list.id,
+            taskIds: taskId,
+          });
+          console.log(lists);
+          const map = new Map(lists);
+          setData({ lists: map, tasks });
+        }
+      }
+    }
   };
   return (
     <div style={{ display: "flex", justifyContent: "space-around" }}>
       {
-        <DataProvider value={{ lists, tasks }}>
+        <DataProvider value={data}>
           <DragDropContext onDragEnd={onDragEnd}>{listsViews}</DragDropContext>
         </DataProvider>
       }
