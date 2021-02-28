24dd4b8491e1f8047f71067b3dd698d98743262c
Thanh Binh Le <ltbinh3999@gmail.com>
2021-02-28 17:52:56 +0700

    Change sample data and remove subtask data

--- src/data.ts
+++ src/data.ts
@@ -18,34 +18,33 @@ export interface Board {
   tasks: Map<string, Task>;
   lists: Map<string, List>;
 }
-
+//TODO: implement subtask Function
 function getBoard(boardId: string) {
   const addSubTask = (taskId: string, subTasksId: string) => {
     tasks.get(taskId)?.subTasks.push(subTasksId);
   };
-  const taskArr: Task[] = Array.from(Array(15).keys()).map((x) => ({
+  const taskArr: Task[] = Array.from(Array(10).keys()).map((x) => ({
     id: x.toString(),
-    name: `task${x}`,
+    name: `Sample task ${x}`,
     subTasks: [],
   }));
   const tasks = new Map<string, Task>();
   taskArr.forEach((x) => {
     tasks.set(x.id, x);
   });
-  addSubTask("0", "1");
-  addSubTask("0", "2");
-  addSubTask("0", "3");
-  addSubTask("1", "4");
-  addSubTask("1", "5");
-  addSubTask("6", "7");
+
   const lists = new Map<string, List>();
   lists.set("l0", {
     id: "l0",
-    name: "List 0",
-    taskIds: ["0", "9", "10", "11"],
+    name: "Sample list 0",
+    taskIds: ["0", "1", "2", "3"],
   });
-  lists.set("l1", { id: "l1", name: "List 1", taskIds: ["6", "8"] });
-  lists.set("l2", { id: "l2", name: "List 2", taskIds: ["12", "13", "14"] });
+  lists.set("l1", { id: "l1", name: "Sample list 1", taskIds: ["4", "5"] });
+  lists.set("l2", {
+    id: "l2",
+    name: "Sample list 2",
+    taskIds: ["6", "7", "8", "9"],
+  });
 
   return {
     id: "b1",
